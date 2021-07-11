986aef6237ef8262b4add2250ecaace102c48092
System Administrator <root@Nishants-MacBook-Air.local>
2021-07-11 10:47:58 +0530

    Compile assets for deployment Rinkeby

--- deployApp.sh
+++ deployApp.sh
@@ -1,5 +1,5 @@
 # rsync -r src/ docs/
 # rsync build/contracts/* docs/
 git add .
-git commit -m "UI updation for DAPP"
+git commit -m "Compile assets for deployment Rinkeby"
 git push
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,10 @@
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "6b1ef2c97f914d1eb35e0fbed1219984";
+const mnemonic = "pact super anger west return vacuum post coconut wolf script where tuition"
+const URL = 'https://rinkeby.infura.io/v3/' + infuraKey
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -7,8 +14,13 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    develop: {
-      port: 8545
+    rinkeby: {
+      networkCheckTimeout: 10000,
+      provider:function() { return new HDWalletProvider(mnemonic, URL);
+      },
+      network_id: 4,
+      gas: 4700000,
+      confirmations: 4 
     }
   }
 };
